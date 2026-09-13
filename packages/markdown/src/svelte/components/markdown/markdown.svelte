ed54078811c8b984d47492fcf7e139cd19fb29ac
vnphanquang <oss@vnphanquang.com>
2026-09-13 14:53:09 +0700

    feat(markdown): skip diff-removed lines when copying

--- packages/markdown/src/svelte/components/markdown/markdown.svelte
+++ packages/markdown/src/svelte/components/markdown/markdown.svelte
@@ -11,7 +11,21 @@
 	}
 
 	onMount(() => {
-		enhanceCodeblock();
+		enhanceCodeblock({
+			copy: {
+				fn({ pre }) {
+					const codeNode = pre.getElementsByTagName('code')[0];
+					if (!codeNode) return '';
+					let text = '';
+					for (const lineNode of codeNode.children) {
+						// assuming ../../../shiki setup
+						if ((lineNode as HTMLElement).classList.contains('remove')) continue;
+						text += (lineNode.textContent || '') + '\n';
+					}
+					return text;
+				},
+			},
+		});
 	});
 </script>
 
