385f127bf933074b8bba4496336a679cda99b855
vnphanquang <oss@vnphanquang.com>
2026-09-20 12:10:08 +0700

    chore(vnphanquang.com): placeholder for rethinking-markdown-in-svelte blog post

---
+++ sites/vnphanquang.com/src/data/posts/entries/en/20260919-rethinking-markdown-in-svelte/thumbnail.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import svelte from '$lib/assets/ascii/svelte.txt?raw';
+	import { Ascii } from '$lib/components/ascii';
+
+	import arrowCircle from './includes/ascii/arrow-circle.txt?raw';
+	import markdown from './includes/ascii/markdown.txt?raw';
+</script>
+
+<div class="flex items-center justify-center gap-4 text-[0.5cqi] @3xl:gap-10" aria-hidden={true}>
+	<Ascii>{markdown}</Ascii>
+	<Ascii class="ml-4">{arrowCircle}</Ascii>
+	<Ascii>{svelte}</Ascii>
+</div>
