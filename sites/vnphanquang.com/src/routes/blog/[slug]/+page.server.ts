44e3f47e246532aba2ae1bb38b460911a279469d
vnphanquang <oss@vnphanquang.com>
2026-09-25 16:53:35 +0700

    chore(vnphanquang.com): turn off prerender for blog pages - require color-scheme cookie

--- sites/vnphanquang.com/src/routes/blog/[slug]/+page.server.ts
+++ sites/vnphanquang.com/src/routes/blog/[slug]/+page.server.ts
@@ -2,5 +2,4 @@ import { generateKitEntries } from '#data/posts';
 
 import type { EntryGenerator } from './$types';
 
-export const prerender = true;
 export const entries: EntryGenerator = generateKitEntries;
